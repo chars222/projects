90a8393218c8b49ed94e6250f315cae200c94db5
cgutierrez <cgutierrez@gruposion.bo>
2019-05-16 15:17:03 -0400

    new changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello UIT' });
+  res.render('index', { title: 'muy app' });
 });
 
 module.exports = router;
